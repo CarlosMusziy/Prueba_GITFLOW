cac3e83b0faaf5df4c7a974be588c63406b9dc7f
carlosMusziy <carlosmusziy@gmail.com>
2023-11-20 09:03:52 -0300

    parece que entendemos

--- milei2011.java
+++ milei2011.java
@@ -2,4 +2,7 @@ public interface milei2011 {
     public String Presidente="milei";
     public String cancion="milei";
     public String perro="pinki";
+
+    public String parece="Parece que entendemos";
+
 }
